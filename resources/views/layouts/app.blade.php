d810462569146bc977505049da10629755a25886
becasl <ninhocnp@gmail.com>
2021-02-28 17:19:24 +0100

    compiling assets2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
         <meta name="googlebot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
         <meta name="bingbot" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
         <!-- Vendor CSS Files -->
-        <link rel="preload" href="assets/vendor/venobox/venobox.css" rel="stylesheet">
-        <link rel="preload" href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
-        <link rel="preload" href="assets/vendor/aos/aos.css" rel="stylesheet">
-        <link rel="preload" href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
-        <link rel="preload" href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+        <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+        <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 
         <!-- Template Main CSS File -->
         <link href="/css/style.css" rel="stylesheet">
@@ -136,15 +136,15 @@
             </div>
 
             <!-- Vendor JS Files -->
-  <script async src="assets/vendor/jquery/jquery.min.js"></script>
-  <script async src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script async src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script async src="assets/vendor/php-email-form/validate.js"></script>
-  <script async src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
-  <script async src="assets/vendor/venobox/venobox.min.js"></script>
-  <script async src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script async src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script async src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
 
   <!-- Template Main JS File -->
   <script async src="js/main.js"></script>
