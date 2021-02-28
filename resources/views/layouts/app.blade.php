a4932894e98610157b8daf8d42d2def950dafaf1
becasl <ninhocnp@gmail.com>
2021-02-28 17:17:58 +0100

    compiling assets1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -136,15 +136,15 @@
             </div>
 
             <!-- Vendor JS Files -->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-  <script src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
-  <script src="assets/vendor/venobox/venobox.min.js"></script>
-  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
-  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
+  <script async src="assets/vendor/jquery/jquery.min.js"></script>
+  <script async src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script async src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script async src="assets/vendor/php-email-form/validate.js"></script>
+  <script async src="assets/vendor/jquery-sticky/jquery.sticky.js"></script>
+  <script async src="assets/vendor/venobox/venobox.min.js"></script>
+  <script async src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script async src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script async src="assets/vendor/aos/aos.js"></script>
 
   <!-- Template Main JS File -->
   <script async src="js/main.js"></script>
